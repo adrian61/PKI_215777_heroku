2e5190f9888d4388e33cf902742233290a791a53
adrian61 <ad.kowalskI@gmail.com>
2020-04-27 21:52:24 +0200

    small fix

--- app.js
+++ app.js
@@ -134,7 +134,7 @@ passport.use(new FacebookStrategy({
 app.get('/auth/facebook', passport.authenticate('facebook', { scope: "email" }));
 
 app.get('/auth/facebook/callback',
-    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    passport.authenticate('facebook', { failureRedirect: 'login' }),
     function (req, res) {
         authed = true;
         socialService = "facebook";
